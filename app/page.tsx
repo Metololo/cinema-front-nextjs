91c55447fa309e5ac703355e4b1bc56aa2c431ef
MetooSHEESH <93775692+Metololo@users.noreply.github.com>
2024-03-29 21:25:46 +0100

    feat: setup shadcn themes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <h1>HOME</h1>;
+  return (
+    <div>
+      <main className="w-full bg-red-500 min-h-[100vh] "></main>
+    </div>
+  );
 }
